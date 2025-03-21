138ec020887784c9cab630f8e1f70341751507bf
VIJITH S B <125852753+Vijith06@users.noreply.github.com>
2025-03-21 03:56:18 +0000

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -2,8 +2,8 @@
     echo hi123
     sh 'chmod +x build.sh'
     sh './build.sh'
-    docker login -u ar8888 -p dckr_pat_HU8f1bsQJp3j2DnCttXuuoppo7w
-    docker tag test ar8888/dev
-    docker push ar8888/dev
-    docker-compose up -d
+    docker login -u vijith22csr239 -p dckr_pat_l1cCBOyiqj0VcW1u9CxW9C3M19M
+    docker tag test vijith22csr239/dev
+    docker push vijith22csr239/dev
+    # docker-compose up -d
     
